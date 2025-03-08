cde66318f0b2b6c00d2e49c69ef8c954697c5e96
sergiosja <sergey.jakobsen@gmail.com>
2025-03-08 17:13:10 +0100

    added delete method

--- server/Api/Resources/ResourceHandler.fs
+++ server/Api/Resources/ResourceHandler.fs
@@ -4,6 +4,10 @@ open Giraffe
 open Dapper
 open Npgsql
 
+// For the time being, I cannot open Api.StatusCodes, for some reason. So I have these locally:
+let withStatusCode statusCode msg = setStatusCode statusCode >=> text msg
+let notFound = withStatusCode 404
+
 type Resource = {
     id: int
     title: string
@@ -14,10 +18,15 @@ let connectDb () =
     let connectionString = System.Environment.GetEnvironmentVariable("CONNECTION_STRING")
     new NpgsqlConnection(connectionString)
 
-let handleNull r =
+let handleGetNull r =
     match box r with
     | null -> None
     | _ -> Some r
+
+let handleDeleteNull id =
+    match id with
+    | 0 -> None
+    | _ -> Some id
 
 
 // GET
@@ -39,7 +48,7 @@ let getResourcesHandler : HttpHandler =
             let! maybeResources = getResources
             let handler =
                 match maybeResources with
-                | [] -> setStatusCode 404 >=> text "No resources"
+                | [] -> notFound "Could not find any resources."
                 | resources -> json resources
 
             return! handler next ctx
@@ -56,7 +65,7 @@ let getResourceById id =
                 {| Id = id |}
             )
 
-        return handleNull result
+        return handleGetNull result
     }
 
 let getResourceByIdHandler id : HttpHandler =
@@ -66,7 +75,7 @@ let getResourceByIdHandler id : HttpHandler =
             let handler =
                 match maybeResource with
                 | Some result -> json result
-                | None -> setStatusCode 404 >=> text "ResourceNotFound"
+                | None -> notFound "Could not find any resource with provided id."
 
             return! handler next ctx
         }
@@ -93,4 +102,32 @@ let postResourceHandler : HttpHandler =
             let! resource = ctx.BindJsonAsync<Resource>()
             let! id = postResource resource
             return! json {| id = id |} next ctx
+        }
+
+
+// DELETE
+
+let deleteResource id =
+    task {
+        use conn = connectDb()
+        let! deletedId = conn.QueryFirstOrDefaultAsync<int>(
+            "DELETE FROM resource
+            WHERE id = @Id
+            RETURNING id",
+            {| Id = id |}
+        )
+
+        return handleDeleteNull deletedId        
+    }
+
+let deleteResourceHandler id : HttpHandler =
+    fun next ctx ->
+        task {
+            let! deletedResourceId = deleteResource id
+            let handler =
+                match deletedResourceId with
+                | Some id -> json {| id = id |}
+                | None -> notFound "Could not find resource to delete."
+
+            return! handler next ctx
         }
