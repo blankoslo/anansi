d57007d939e48a96ce7c171768ea0c70d37a0db5
sergiosja <sergey.jakobsen@gmail.com>
2025-03-08 19:43:48 +0100

    more status codes

--- server/Api/StatusCodes.fs
+++ server/Api/StatusCodes.fs
@@ -5,6 +5,13 @@ open Giraffe
 let withStatusCode statusCode message =
     setStatusCode statusCode >=> text message
 
+// 200
+let ok message = withStatusCode 200 message
+let created message = withStatusCode 201 message
+let accepted message = withStatusCode 202 message
+let noContent message = withStatusCode 204 message
+
+// 400
 let badRequest message = withStatusCode 400 message
 let unauthorized message = withStatusCode 401 message
 let forbidden message = withStatusCode 403 message
